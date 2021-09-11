e379a78d1af28ec2c2b79d6bf6bd6961b1ace108
Temur Esanov <temuresanov01@gmail.com>
2021-09-11 08:50:11 +0200

    разделил код и картинки файлом

--- js/galleryItems.js
+++ js/galleryItems.js
@@ -1,4 +1,5 @@
-const galleryItems = [
+export default 
+[
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -63,68 +64,3 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
-// ===== ADD MARKUP =====
-
-const gallery = document.querySelector('.gallery');
-const largeImage = document.querySelector('.lightbox__image');
-const modal = document.querySelector('.lightbox');
-const closeBtn = document.querySelector('[data-action="close-lightbox"]');
-const overlay = document.querySelector('.lightbox__overlay');
-
-
-const galleryMarkup = [];
-for (let galleryItem of galleryItems) {
-  galleryMarkup.push(`<li class ="gallery__item">
-   <a class="gallery__link" href="${galleryItem.original}">
-    <img
-      class="gallery__image"
-      src="${galleryItem.preview}" 
-      alt="${galleryItem.description}"
-      data-source="${galleryItem.original}"
-      />
-  </a>
-  </li>`);
-};
-document.querySelector("ul").insertAdjacentHTML("beforeEnd", [...galleryMarkup].join(""));
-
-// ===== OPEN MODAL =====
-
-const openModal = () => {
-  modal.classList.add('is-open');
-  overlay.addEventListener('click', onOverlayClick);
-  window.addEventListener('keydown', onEscPress); //cLOSE MODAL USING ESC
-  };
-
-// ===== CLOSE MODAL =====
-
-const closeModal = () => {
-  modal.classList.remove('is-open');
-  overlay.removeEventListener('click', onOverlayClick);
-  window.removeEventListener('keydown', onEscPress); //cLOSE MODAL USING ESC
-};
-
-const onGalleryClick = event => {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  let imageRef = event.target;
-  let largeImageURL = imageRef.dataset.source;
-  largeImage.src = largeImageURL;
-   openModal();
-};
-
-const onOverlayClick = event =>
-  event.currentTarget === event.target ? closeModal() : '';
-
-gallery.addEventListener('click', onGalleryClick);
-closeBtn.addEventListener('click', closeModal);
-
-// ===== CLOSE MODAL USING ESC =====
-
-const onEscPress = event => {
-  if (event.code === 'Escape') {
-    closeModal();
-  }
-}
